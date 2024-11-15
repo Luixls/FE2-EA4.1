567856b4b72c25ee869feb36c862d011c118ee3e
FN2814 <cardozaferrerje@uvm.edu.ve>
2024-11-15 16:32:45 -0400

    Anexo de funcionalidad como PWA
    Co-Authored-By: Luixls <56209449+Luixls@users.noreply.github.com>
    Co-Authored-By: jonathanuvm <127060318+jonavnet@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,20 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Registrar el Service Worker para convertir la aplicación en una PWA
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((registration) => {
+        console.log("Service Worker registrado con éxito:", registration);
+      })
+      .catch((error) => {
+        console.log("Fallo al registrar el Service Worker:", error);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
