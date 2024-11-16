dc449cd9ae460eaca73a392cdd8f8376d0f8e7a7
Luixls <56209449+Luixls@users.noreply.github.com>
2024-11-16 16:44:57 -0400

    Pequeño fix
    Actualizando el comentario placeholder de agregar tiempos en competencias
    
    Co-Authored-By: jonathanuvm <127060318+jonavnet@users.noreply.github.com>
    Co-Authored-By: FN2184 <159496056+FN2184@users.noreply.github.com>

--- src/pages/Competencias.jsx
+++ src/pages/Competencias.jsx
@@ -362,7 +362,7 @@ function Competencias() {
                     </select>
                     <input
                       type="text"
-                      placeholder="Tiempo (ej: 1min 3seg)"
+                      placeholder="Tiempo (ej: 1h 33m 55s)"
                       value={participante.tiempo}
                       onChange={(e) =>
                         handleParticipantChange(index, "tiempo", e.target.value)
